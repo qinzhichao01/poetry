2c9d896f2138eb9776e5bdde7e21ef10247659ef
qinzhichao02 <qinzhichao02@meituan.com>
2023-04-23 13:54:14 +0800

    hot100

--- src/com/qinzhichao/hot100/H128_最长连续序列.java
+++ src/com/qinzhichao/hot100/H128_最长连续序列.java
@@ -34,34 +34,4 @@ public class H128_最长连续序列 {
         return res;
     }
 }
-class Solution {
-    public int longestConsecutive(int[] nums) {
-        Set<Integer> num_set = new HashSet<Integer>();
-        for (int num : nums) {
-            num_set.add(num);
-        }
 
-        int longestStreak = 0;
-
-        for (int num : num_set) {
-            if (!num_set.contains(num - 1)) {
-                int currentNum = num;
-                int currentStreak = 1;
-
-                while (num_set.contains(currentNum + 1)) {
-                    currentNum += 1;
-                    currentStreak += 1;
-                }
-
-                longestStreak = Math.max(longestStreak, currentStreak);
-            }
-        }
-
-        return longestStreak;
-    }
-}
-
-作者：LeetCode-Solution
-        链接：https://leetcode.cn/problems/longest-consecutive-sequence/solution/zui-chang-lian-xu-xu-lie-by-leetcode-solution/
-        来源：力扣（LeetCode）
-        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
