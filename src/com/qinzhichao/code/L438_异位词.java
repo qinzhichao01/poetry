6ad729a8b1be040f43d9e99387e559f632b70f0e
qinzhichao02 <2692277504@qq.com>
2023-04-25 08:06:44 +0800

    leetcode

---
+++ src/com/qinzhichao/code/L438_异位词.java
@@ -0,0 +1,102 @@
+package com.qinzhichao.code;
+
+import java.util.*;
+
+/**
+ * @author qinzhichao
+ * created at 2023/4/25 01:44
+ **/
+public class L438_异位词 {
+    public List<Integer> findAnagrams(String s, String p) {
+        Map<Character, Integer> need = new HashMap<>();
+        char[] charArray = p.toCharArray();
+        for (char c : charArray) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+        int left = 0;
+        int right = 0;
+        int count = 0;
+        List<Integer> result = new ArrayList<>();
+        Map<Character, Integer> window = new HashMap<>();
+        char[] charArray2 = s.toCharArray();
+        while (right < charArray2.length) {
+            char c = charArray2[right];
+            right++;
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+
+                if (window.get(c).equals(need.get(c))) {
+                    count++;
+                    if (count == need.size()) {
+                        result.add(left);
+                        char d = charArray2[left];
+                        window.put(d, window.get(d) - 1);
+                        left++;
+                        count--;
+                    }
+                } else if (window.get(c) > need.get(c)) {
+                    char d = charArray2[left];
+                    Set<Character> set = new HashSet<>();
+                    while (d != c) {
+                        window.put(d, window.get(d) - 1);
+                        if (!set.contains(d)) {
+                            set.add(d);
+                            count--;
+                        }
+                        left++;
+                        d = charArray2[left];
+                    }
+                    window.put(c, need.get(c));
+                    left++;
+                }
+            } else {
+                left = right;
+                window.clear();
+                count = 0;
+            }
+        }
+        return result;
+    }
+
+    public List<Integer> findAnagrams(String s, String p) {
+        // 用于返回字母异位词的起始索引
+        List<Integer> res = new ArrayList<>();
+        // 用 map 存储目标值中各个单词出现的次数
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (Character c : p.toCharArray()) map.put(c, map.getOrDefault(c, 0) + 1);
+        // 用另外一个 map 存储滑动窗口中有效字符出现的次数
+        HashMap<Character, Integer> window = new HashMap<>();
+        int left = 0; // 左指针
+        int right = 0; // 右指针
+        int valid = p.length(); // 只有当 valid == 0 时，才说明 window 中包含了目标子串
+        while (right < s.length()) {
+            // 如果目标子串中包含了该字符，才存入 window 中
+            if (map.containsKey(s.charAt(right))) {
+                window.put(s.charAt(right), window.getOrDefault(s.charAt(right), 0) + 1);
+                // 只有当 window 中该有效字符数量不大于map中该字符数量，才能算一次有效包含
+                if (window.get(s.charAt(right)) <= map.get(s.charAt(right))) {
+                    valid--;
+                }
+            }
+            // 如果 window 符合要求，即两个 map 存储的有效字符相同，就可以移动左指针了
+            // 但是只有二个map存储的数据完全相同，才可以记录当前的起始索引，也就是left指针所在位置
+            while (valid == 0) {
+                if (right - left + 1 == p.length()) res.add(left);
+                // 如果左指针指的是有效字符,需要更改 window 中的 key 对应的 value
+                // 如果 有效字符对应的数量比目标子串少，说明无法匹配了
+                if (map.containsKey(s.charAt(left))) {
+                    window.put(s.charAt(left), window.get(s.charAt(left)) - 1);
+                    if (window.get(s.charAt(left)) < map.get(s.charAt(left))) {
+                        valid++;
+                    }
+                }
+                left++;
+            }
+            right++;
+        }
+        return res;
+    }
+}
+
+
+
