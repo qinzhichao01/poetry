450b8ca0996322cae553ab28a71f0d34574d7275
anzunming <an2190881697@163.com>
2023-04-29 03:04:42 +0800

    最大子数组和-正确解答 执行用时：88 ms, 在所有 C++ 提交中击败了72.21%的用户 内存消耗：68.7 MB, 在所有 C++ 提交中击败了19.28%的用户 通过测试用例：210 / 210

--- src/com/leetcode/editor/cn/[53]最大子数组和.cpp
+++ src/com/leetcode/editor/cn/[53]最大子数组和.cpp
@@ -43,10 +43,27 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class Solution
+{
 public:
-    int maxSubArray(vector<int>& nums) {
+    int maxSubArray(vector<int> &nums)
+    {
+        int size = nums.size();
+        if (0 == size)
+        {
+            return 0;
+        }
 
+        // 使用动态规划
+        vector<int> dp(size);
+        int res = nums[0];
+        dp[0] = nums[0];
+        for (int i = 1; i < size; ++i)
+        {
+            dp[i] = (nums[i] + dp[i - 1]) > nums[i] ? (nums[i] + dp[i - 1]) : nums[i];
+            res = res > dp[i] ? res : dp[i];
+        }
+        return res;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
