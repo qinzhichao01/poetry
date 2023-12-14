b42bad5be46a2b7de7756687fb523628a71c4ea2
anzunming <an2190881697@163.com>
2023-12-14 23:49:05 +0800

    [700]二叉搜索树中的搜索 解答成功: 	执行耗时:40 ms,击败了44.65% 的C++用户 	内存消耗:34.2 MB,击败了63.56% 的C++用户

---
+++ src/com/leetcode/editor/cn/[700]二叉搜索树中的搜索.cpp
@@ -0,0 +1,73 @@
+//给定二叉搜索树（BST）的根节点
+// root 和一个整数值
+// val。 
+//
+// 你需要在 BST 中找到节点值等于 val 的节点。 返回以该节点为根的子树。 如果节点不存在，则返回
+// null 。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+// 
+//
+// 
+//输入：root = [4,2,7,1,3], val = 2
+//输出：[2,1,3]
+// 
+//
+// 示例 2: 
+// 
+// 
+//输入：root = [4,2,7,1,3], val = 5
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数在 [1, 5000] 范围内 
+// 1 <= Node.val <= 10⁷ 
+// root 是二叉搜索树 
+// 1 <= val <= 10⁷ 
+// 
+//
+// Related Topics 树 二叉搜索树 二叉树 👍 453 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *searchBST(TreeNode *root, int val) {
+        if (nullptr == root) {
+            return nullptr;
+        }
+
+        auto p = root;
+        while (p) {
+            if (p->val == val) {
+                return p;
+            } else if (p->val > val) {
+                p = p->left;
+            } else {
+                p = p->right;
+            }
+        }
+        return nullptr;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
