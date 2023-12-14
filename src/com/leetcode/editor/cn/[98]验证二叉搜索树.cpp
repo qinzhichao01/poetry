ac1469cad11d62f9c29b9778885f1df037ac1397
anzunming <an2190881697@163.com>
2023-12-14 22:39:56 +0800

    [98]验证二叉搜索树 解答成功: 	执行耗时:8 ms,击败了88.69% 的C++用户 	内存消耗:21.9 MB,击败了5.86% 的C++用户

---
+++ src/com/leetcode/editor/cn/[98]验证二叉搜索树.cpp
@@ -0,0 +1,75 @@
+//给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。 
+//
+// 有效 二叉搜索树定义如下： 
+//
+// 
+// 节点的左子树只包含 小于 当前节点的数。 
+// 节点的右子树只包含 大于 当前节点的数。 
+// 所有左子树和右子树自身必须也是二叉搜索树。 
+// 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [2,1,3]
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [5,1,4,null,null,3,6]
+//输出：false
+//解释：根节点的值是 5 ，但是右子节点的值是 4 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围在[1, 10⁴] 内 
+// -2³¹ <= Node.val <= 2³¹ - 1 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 2234 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode *root) {
+        if (nullptr == root) {
+            return true;
+        }
+
+        auto left = isValidBST(root->left);
+        if (!m_stack.empty()) {
+            auto temp = m_stack.top();
+            m_stack.pop();
+            if (temp >= root->val) {
+                return false;
+            }
+        }
+        m_stack.push(root->val);
+        auto right = isValidBST(root->right);
+        return left & right;
+    }
+
+private:
+    stack<int> m_stack;
+};
+//leetcode submit region end(Prohibit modification and deletion)
